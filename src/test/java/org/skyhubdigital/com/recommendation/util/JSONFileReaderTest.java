9f999868f5cc5df4390d701ace40410752c0b369
Mohanasundaram Govindarajan <ggmohan@gmail.com>
2016-09-01 12:15:20 +1000

    Update JSONFileReaderTest.java
    updated Test

--- src/test/java/org/skyhubdigital/com/recommendation/util/JSONFileReaderTest.java
+++ src/test/java/org/skyhubdigital/com/recommendation/util/JSONFileReaderTest.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
 /**
  * Created by MohanDivya on 8/31/2016.
@@ -21,16 +20,5 @@ public class JSONFileReaderTest {
         assertThat(movies.length,is(20));
     }
 
-    @Test
-    public void shouldReadIfRunTimeIsMoreThanZero() throws IOException, ParseException {
-        Movie[] movies = JSONFileReader.readMovieFile();
-        assertThat(movies.length,is(20));
 
-        for (Movie movie : movies) {
-
-            if(movie!=null){
-                assertTrue(movie.getDuration() > 0);
-            }
-        }
-    }
 }
